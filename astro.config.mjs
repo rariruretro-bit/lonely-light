3b66afb0dc7ca8693bcf948bb50ef75ce508fdc7
rarir <rariruretro@gmail.com>
2026-07-14 09:10:15 +0900

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://あなたのGitHubユーザー名.github.io',
+  base: '/lonely-light',
+});
